909bc5451dd335c51ee84a4f74699f89a3e056f2
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2022-10-06 14:47:51 -0400

    options page functionality

--- api/src/routes/options.ts
+++ api/src/routes/options.ts
@@ -3,6 +3,7 @@ import Service from '../models/Service'
 import ServiceEventType from '../models/ServiceEventType'
 import ServiceRole from '../models/ServiceRole'
 import ServiceType from '../models/ServiceType'
+import randomColor from 'randomcolor'
 const router = express.Router()
 
 // Event Type Routes
@@ -28,8 +29,8 @@ router.post('/events/delete', async (req: Request, res: Response) => {
   // Do not delete role if a service uses that event
   const existing = await Service
     .findOne({
-      'events': {
-        $elemMatch: { 'type': _id }
+      events: {
+        $elemMatch: { type: _id }
       }
     })
   if (existing) {
@@ -73,8 +74,8 @@ router.post('/roles/delete', async (req: Request, res: Response) => {
   // Do not delete role if a service uses that role
   const existing = await Service
     .findOne({
-      'people': {
-        $elemMatch: { 'role': _id }
+      people: {
+        $elemMatch: { role: _id }
       }
     })
   if (existing) {
@@ -102,7 +103,20 @@ router.get('/types', async (req: Request, res: Response) => {
 })
 
 router.post('/types/new', async (req: Request, res: Response) => {
-  res.send('type: ' + req.body.type)
+  const background: string = randomColor()
+
+  const type = new ServiceType({
+    type: req.body.type,
+    color: getContrast(hexToRgb(background)),
+    background
+  })
+  try {
+    const newType = await type.save()
+    res.send(newType)
+  } catch (err) {
+    console.log(err)
+    res.status(400).send({ err })
+  }
 })
 
 router.post('/types/update', async (req: Request, res: Response) => {
@@ -110,8 +124,44 @@ router.post('/types/update', async (req: Request, res: Response) => {
 })
 
 router.post('/types/delete', async (req: Request, res: Response) => {
-  res.send('id: ' + req.body.id)
+  const _id: string = req.body.id
+
+  // Do not delete role if a service uses that event
+  const existing = await Service.findOne({ type: _id })
+  if (existing) {
+    return res.status(400).send('Cannot delete service type that is currently assigned to a service. Please delete any uses of this service type first.')
+  }
+
+  // Delete role
+  try {
+    await ServiceEventType.deleteOne({ _id })
+    res.send('Success')
+  } catch (err) {
+    let message = ''
+    if (err instanceof Error) message = err.message
+    else message = 'An unknown error occured'
+    res.status(400).send(message)
+  }
 })
+
+
+
+type rgbType = [number, number, number]
+
+function hexToRgb(hex: string): rgbType {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? [
+    parseInt(result[1], 16),
+    parseInt(result[2], 16),
+    parseInt(result[3], 16)
+  ] : [0, 0, 0]
+}
+
+// gets text color for background
+function getContrast(rgb: rgbType) {
+  const brightness = Math.round((rgb[0] * 299) + (rgb[1] * 587) + (rgb[2] * 114)) / 1000
+  return (brightness > 125) ? '#000000' : '#FFFFFF'
+}
 
 
 
