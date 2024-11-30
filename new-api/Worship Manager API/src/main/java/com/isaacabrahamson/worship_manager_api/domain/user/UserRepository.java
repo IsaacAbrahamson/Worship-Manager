16207b581bd379acb103f7d5a440a3c42389bfe1
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2024-11-30 15:20:53 -0500

    remove new api code

--- new-api/Worship Manager API/src/main/java/com/isaacabrahamson/worship_manager_api/domain/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.isaacabrahamson.worship_manager_api.domain.user;
-
-import com.isaacabrahamson.worship_manager_api.domain.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
