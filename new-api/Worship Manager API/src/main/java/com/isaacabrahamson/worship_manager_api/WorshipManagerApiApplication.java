1bdb792921354922e49b46302360935e4369de30
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2024-11-30 12:27:00 -0500

    Add new java api

---
+++ new-api/Worship Manager API/src/main/java/com/isaacabrahamson/worship_manager_api/WorshipManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.isaacabrahamson.worship_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorshipManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorshipManagerApiApplication.class, args);
+	}
+
+}
